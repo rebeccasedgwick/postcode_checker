6fa59c89fa1c509e01b2f969c81ee519eac8a6f1
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2019-11-03 15:21:38 +0000

    Tidy tests: standardise postcodes used

--- spec/features/user_checks_postcode_spec.rb
+++ spec/features/user_checks_postcode_spec.rb
@@ -4,12 +4,12 @@ RSpec.feature 'Check a postcode', type: :feature do
   scenario 'User checks a whitelisted postcode' do
     WhitelistedPostcode.create!(postcode: 'sh241aa')
     visit root_path
-    fill_in 'Postcode', with: 'sh241aa'
+    fill_in 'Postcode', with: 'SH24 1AA'
     click_button 'Check postcode'
     expect(page).to have_text('Great news, this postcode is in our service area')
   end
 
-  scenario 'User checks an unserviced, non-whitelisted postcode' do
+  scenario 'User checks an invalid postcode' do
     visit root_path
     fill_in 'Postcode', with: 'invalid'
     VCR.use_cassette('postcodes-io-invalid') do
@@ -18,10 +18,19 @@ RSpec.feature 'Check a postcode', type: :feature do
     expect(page).to have_text("Unfortunately we couldn't find this postcode in our service area")
   end
 
+  scenario 'User checks an unserviced, non-whitelisted postcode' do
+    visit root_path
+    fill_in 'Postcode', with: 'TR19 7AA'
+    VCR.use_cassette('postcodes-io-postcode_not_serviced') do
+      click_button 'Check postcode'
+    end
+    expect(page).to have_text("Unfortunately we couldn't find this postcode in our service area")
+  end
+
   scenario 'User checks a serviced LSOA area' do
     visit root_path
-    fill_in 'Postcode', with: 'se13gb'
-    VCR.use_cassette('postcodes-io-valid') do
+    fill_in 'Postcode', with: 'SE1 7QD'
+    VCR.use_cassette('postcodes-io-whitelisted-lsoa') do
       click_button 'Check postcode'
     end
     expect(page).to have_text('Great news, this postcode is in our service area')
--- spec/lib/services/postcode_validator_spec.rb
+++ spec/lib/services/postcode_validator_spec.rb
@@ -3,70 +3,74 @@
 require 'rails_helper'
 
 RSpec.describe PostcodeValidator do
-  let(:subject) do
-    described_class.new('SE1 3GB')
-  end
+  let(:subject_whitelisted_lsoa) { described_class.new('SE1 7QD') }
+  let(:subject_whitelisted_postcode) { described_class.new('SH24 1AA') }
+  let(:subject_invalid_postcode) { described_class.new('INVALID') }
+  let(:subject_valid_postcode_not_serviced) { described_class.new('TR19 7AA') }
 
   describe 'initialize' do
     it 'normalises the postcode' do
-      expect(subject.postcode).to eq('se13gb')
+      expect(subject_whitelisted_lsoa.postcode).to eq('se17qd')
     end
   end
 
   describe '#valid?' do
-    it 'returns true if postcode is whitelisted' do
-      expect(subject).to receive(:whitelisted_postcode?).and_return(true)
-      expect(subject.valid?).to eq(true)
+    context 'when the postcode is whitelisted' do
+      before do
+        WhitelistedPostcode.create!(postcode: 'SH24 1AA')
+      end
+      it 'returns true if postcode is whitelisted' do
+        expect(subject_whitelisted_postcode).to receive(:whitelisted_postcode?).and_return(true)
+        expect(subject_whitelisted_postcode.valid?).to eq(true)
+      end
     end
 
     context 'if postcode is found by API' do
       it 'returns true if LSOA is whitelisted' do
-        VCR.use_cassette('postcodes-io-valid') do
-          expect(subject).to receive(:whitelisted_lsoa?).and_return(true)
-          expect(subject.valid?).to eq(true)
+        VCR.use_cassette('postcodes-io-whitelisted-lsoa') do
+          expect(subject_whitelisted_lsoa).to receive(:whitelisted_lsoa?).and_return(true)
+          expect(subject_whitelisted_lsoa.valid?).to eq(true)
         end
       end
       it 'returns false if LSOA is not whitelisted' do
-        VCR.use_cassette('postcodes-io-valid') do
-          expect(subject).to receive(:whitelisted_lsoa?).and_return(false)
-          expect(subject.valid?).to eq(false)
+        VCR.use_cassette('postcodes-io-postcode_not_serviced') do
+          expect(subject_valid_postcode_not_serviced).to receive(:whitelisted_lsoa?).and_return(false)
+          expect(subject_valid_postcode_not_serviced.valid?).to eq(false)
         end
       end
     end
 
     it 'returns false if the postcode is not found by API' do
-      subject = described_class.new('INVALID')
-      VCR.use_cassette("postcodes-io-invalid") do
-        expect(subject.valid?).to eq(false)
+        VCR.use_cassette('postcodes-io-invalid') do
+        expect(subject_invalid_postcode.valid?).to eq(false)
       end
     end
   end
 
-  describe '#whitelisted_postcode??' do
-    before do
-      WhitelistedPostcode.create!(postcode: 'se13gb')
-    end
-
-    it 'returns true if the postcode is whitelisted' do
-      expect(subject.whitelisted_postcode?).to eq(true)
+  describe '#whitelisted_postcode?' do
+    context 'when the postcode is whitelisted' do
+      before do
+        WhitelistedPostcode.create!(postcode: 'SH24 1AA')
+      end
+      it 'returns true if the postcode is whitelisted' do
+        expect(subject_whitelisted_postcode.whitelisted_postcode?).to eq(true)
+      end
     end
     it 'returns false if the postcode is not whitelisted' do
-      subject = described_class.new('invalid')
-      expect(subject.whitelisted_postcode?).to eq(false)
+      expect(subject_valid_postcode_not_serviced.whitelisted_postcode?).to eq(false)
     end
   end
 
   describe '#fetch_postcode_details' do
     it 'returns parsed JSON response if response is valid' do
-      VCR.use_cassette('postcodes-io-valid') do
-        json = subject.fetch_postcode_details
+      VCR.use_cassette('postcodes-io-whitelisted-lsoa') do
+        json = subject_whitelisted_lsoa.fetch_postcode_details
         expect(json['result']['country']).to eq('England')
       end
     end
     it 'returns nil if response is not valid' do
-      subject = described_class.new('INVALID')
       VCR.use_cassette('postcodes-io-invalid') do
-        value = subject.fetch_postcode_details
+        value = subject_invalid_postcode.fetch_postcode_details
         expect(value).to eq(nil)
       end
     end
@@ -77,10 +81,11 @@ RSpec.describe PostcodeValidator do
       stub_const('PostcodeValidator::LSOA_WHITELIST', ['Lambeth', 'Southwark'])
     end
     it 'returns true if LSOA is whitelisted' do
-      expect(subject.whitelisted_lsoa?('Lambeth')).to eq(true)
+      expect(subject_whitelisted_lsoa.whitelisted_lsoa?('Southwark
+        034A')).to eq(true)
     end
     it 'returns false if LSOA is not whitelisted' do
-      expect(subject.whitelisted_lsoa?('Greenland')).to eq(false)
+      expect(subject_valid_postcode_not_serviced.whitelisted_lsoa?('Cornwall 069C')).to eq(false)
     end
   end
 end
